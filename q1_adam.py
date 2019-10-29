d3e9c894c30c12fb83f7cae41ab6afb7c537e6c1
Adam Azoulay <adamazoulay@live.ca>
2019-10-29 17:00:58 -0400

    Q1 done

--- q1_adam.py
+++ q1_adam.py
@@ -1,3 +1,13 @@
 
 if __name__=="__main__":
-    pass
+    max = 1000
+    total = 0
+
+    # Stop if we find divisible by 3, else check for 5
+    for num in range(max):
+        if num % 3 == 0:
+            total += num
+        elif num % 5 == 0:
+            total += num
+
+    print(total)
