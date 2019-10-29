1d18280f5f1ad453079f1c38b38d6bc6733df17f
Adam Azoulay <adamazoulay@live.ca>
2019-10-29 17:38:18 -0400

    q2 adam

--- q2_adam.py
+++ q2_adam.py
@@ -1,4 +1,16 @@
 # Template file for Project Euler questions
 
 if __name__=="__main__":
-    pass
+    lastfib = 1
+    curfib = 2
+    total = 0
+    while curfib <= 4e6:
+        if curfib % 2 == 0:
+            total += curfib
+
+        # Iterate fib vars
+        newfib = lastfib + curfib
+        lastfib = curfib
+        curfib = newfib
+
+    print(total)
