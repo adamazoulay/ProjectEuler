f0d56e757549ba943f4e320edd14d7cbede0b2ee
Jason Carter <jason_carter141@hotmail.com>
2019-10-29 15:29:17 -0600

    Q1 Upload - Solved

--- q1_jason.py
+++ q1_jason.py
@@ -1,4 +1,13 @@
-# Template file for Project Euler questions
 
 if __name__=="__main__":
-    pass
+    index = 334
+    max = 1000
+    sum = 0
+
+    for x in range(index):
+        if x * 3 < max:
+            sum += x*3
+        if (x * 5 < max) and ((x * 5) % 3 != 0):
+            sum += x*5
+        print(sum)
+    print(sum)
