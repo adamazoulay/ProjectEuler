1f2a7c7b58c9f7aa3bf7e4b212672a9eac9c46dc
Adam Azoulay <adamazoulay@live.ca>
2019-10-30 00:10:47 -0400

    q3 done

--- q3_adam.py
+++ q3_adam.py
@@ -1,4 +1,18 @@
 # Template file for Project Euler questions
+def is_prime(num):
+    for val in range(2, num):
+        if num % val == 0:
+            return False
+    return True
 
 if __name__=="__main__":
-    pass
+    target = 600851475143
+    prime = False
+    test = 1
+    while not prime:
+        test += 1
+        if target % test == 0:
+            check = int(target / test)
+            prime = is_prime(check)
+
+    print(check)
