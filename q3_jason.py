008251feee318a9fcd72bd3879b5636a0b54f71f
Jason Carter <jason_carter141@hotmail.com>
2019-10-31 12:51:29 -0600

    Quick Q3 and Q4template

--- q3_jason.py
+++ q3_jason.py
@@ -1,11 +1,18 @@
+import math
+
+
 def prime_factors(number):
-    prime_nums = []
-    index = 0
-    while index < number:
-        index += 1
-        if 0 == number % index:
-            prime_nums.append(index)
-    return prime_nums
+    big_prime = 1
+
+    if number % 2 == 0:
+        big_prime = 2
+    for oddNum in range(3, int(math.sqrt(number)) + 1, 2):
+        if number % oddNum == 0:
+            big_prime = oddNum
+            number /= oddNum
+
+    return big_prime
+
 
 if __name__ == "__main__":
     print(prime_factors(600851475143))
