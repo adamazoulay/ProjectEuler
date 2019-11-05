140932bd8d579acdebe1360e49d6ba29f16e7201
Adam Azoulay <adamazoulay@live.ca>
2019-11-05 12:47:29 -0500

    q24 done

--- adam/q24_adam.py
+++ adam/q24_adam.py
@@ -1,4 +1,6 @@
-# Template file for Project Euler questions
+# Kinda cheating but it's python so fuck it
+import itertools
 
 if __name__=="__main__":
-    pass
+    perms = list(itertools.permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(perms[999999])
